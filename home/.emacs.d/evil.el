446f51be60a10c2e8611bf7cd782ab8ea8303ac8
Jeff Schoolcraft <jeff@thequeue.net>
2015-10-30 10:54:18 -0400

    move around windows like vim

--- home/.emacs.d/evil.el
+++ home/.emacs.d/evil.el
@@ -131,5 +131,16 @@ Repeated invocations toggle between the two most recently open buffers."
 (define-key evil-normal-state-map (kbd ";") 'evil-ex)
 (define-key evil-visual-state-map (kbd ";") 'evil-ex)
 (define-key evil-motion-state-map (kbd ";") 'evil-ex)
+
+
+;; moving around windows
+(eval-after-load "evil"
+  '(progn
+     (define-key evil-normal-state-map (kbd "C-h") 'evil-window-left)
+     (define-key evil-normal-state-map (kbd "C-j") 'evil-window-down)
+     (define-key evil-normal-state-map (kbd "C-k") 'evil-window-up)
+     (define-key evil-normal-state-map (kbd "C-l") 'evil-window-right)))
+
+
 (provide 'evil)
 ;;; evil.el ends here
