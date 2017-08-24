5cbccd00bb6dc1fc4499300c3e017ea5d5d86574
Jeff Schoolcraft <jeff@thequeue.net>
2017-08-24 09:16:32 -0400

    defining screens, maybe i'll replace moom with this one day

--- home/.hammerspoon/init.lua
+++ home/.hammerspoon/init.lua
@@ -3,3 +3,8 @@ local reloader = require 'reloader'
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "W", function()
   hs.notify.new({title="Hammerspoon", informativeText="Hello World"}):send()
 end)
+
+-- Screens
+local main_monitor = "SE39UY04"
+local left_monitor = "SyncMaster"
+local right_monitor = "SyncMaster"
