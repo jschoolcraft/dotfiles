8523c9d130a36ae934ff543447da89ae87badb2c
Jeff Schoolcraft <jeff@thequeue.net>
2018-03-04 12:38:47 -0500

    NFI tbh

--- home/.emacs.d/custom.el
+++ home/.emacs.d/custom.el
@@ -14,5 +14,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(flycheck-error ((((class color)) (:underline "Red"))))
+ '(flycheck-error ((((class color) (min-colors 89)) (:background "#cc0000" :foreground "#ffffff" :bold t))))
  '(flycheck-warning ((((class color)) (:underline "Orange")))))
