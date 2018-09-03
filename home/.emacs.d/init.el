e2901766d9b5e853633e72ad55a02817fe91cb7f
Jeff Schoolcraft <jeff@thequeue.net>
2018-09-03 11:48:50 -0400

    more rearranging

--- home/.emacs.d/init.el
+++ home/.emacs.d/init.el
@@ -50,6 +50,8 @@
 
 (setq echo-keystrokes 0.25) ;display unfinished keystroke more quickly (defaults 1 second)
 
+(use-package restart-emacs)
+
 (use-package evil
   :defer nil
   :init
@@ -192,40 +194,21 @@
     ;; helm & projectile
 ; stolen straight from https://github.com/zzamboni/dot-emacs/blob/master/init.org
   (use-package helm
-    :disabled
     :defer 1
     :diminish helm-mode
-    :bind
-    (("C-x C-f"       . helm-find-files)
-     ("C-x C-b"       . helm-buffers-list)
-     ("C-x b"         . helm-multi-files)
-     ("M-x"           . helm-M-x)
-     :map helm-find-files-map
-     ("C-<backspace>" . helm-find-files-up-one-level)
-     ("C-f"           . helm-execute-persistent-action)
-     ([tab]           . helm-ff-RET))
-    :config
-    (defun daedreth/helm-hide-minibuffer ()
-      (when (with-helm-buffer helm-echo-input-in-header-line)
-        (let ((ov (make-overlay (point-min) (point-max) nil nil t)))
-          (overlay-put ov 'window (selected-window))
-          (overlay-put ov 'face
-                       (let ((bg-color (face-background 'default nil)))
-                         `(:background ,bg-color :foreground ,bg-color)))
-          (setq-local cursor-type nil))))
-    (add-hook 'helm-minibuffer-set-up-hook 'daedreth/helm-hide-minibuffer)
-    (setq helm-autoresize-max-height 0
-          helm-autoresize-min-height 40
-          helm-M-x-fuzzy-match t
-          helm-buffers-fuzzy-matching t
-          helm-recentf-fuzzy-match t
-          helm-semantic-fuzzy-match t
-          helm-imenu-fuzzy-match t
-          helm-split-window-in-side-p nil
-          helm-move-to-line-cycle-in-source nil
-          helm-ff-search-library-in-sexp t
-          helm-scroll-amount 8
-          helm-echo-input-in-header-line nil)
+    :custom
+      (helm-autoresize-max-height 0)
+      (helm-autoresize-min-height 40)
+      (helm-M-x-fuzzy-match t)
+      (helm-buffers-fuzzy-matching t)
+      (helm-recentf-fuzzy-match t)
+      (helm-semantic-fuzzy-match t)
+      (helm-imenu-fuzzy-match t)
+      (helm-split-window-in-side-p nil)
+      (helm-move-to-line-cycle-in-source nil)
+      (helm-ff-search-library-in-sexp t)
+      (helm-scroll-amount 8)
+      (helm-echo-input-in-header-line nil)
     :init
     (helm-mode 1))
 
