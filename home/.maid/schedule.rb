89f39b397ee498ccd7ee64fe15f7a37d0e5b767f
Jeff Schoolcraft <jeff@thequeue.net>
2019-03-25 07:18:59 -0400

    using the asdf shim for maid

--- home/.maid/schedule.rb
+++ home/.maid/schedule.rb
@@ -7,14 +7,14 @@ set :output, {
 
 if `hostname -s`.chomp == 'mpHaven'
   every 30.minutes do
-    command 'maid clean --force --rules=~/.maid/rules.torrents.rb'
+    command '/Users/jschoolcraft/.asdf/shims/maid clean --force --rules=~/.maid/rules.torrents.rb'
   end
 end
 
 every 1.hour do
-  command 'maid clean --force'
+  command '/Users/jschoolcraft/.asdf/shims/maid clean --force'
 end
 
 every 1.day, at: '2:30 am' do
-  command 'maid clean --force --rules=~/.maid/daily.rb'
+  command '/Users/jschoolcraft/.asdf/shims/maid clean --force --rules=~/.maid/daily.rb'
 end
