3081819bf08ce8a84d12a418c0d1407f1220ae25
Jeff Schoolcraft <jeff@thequeue.net>
2019-11-08 17:08:19 -0500

    Cleans up downloaded files from working

--- home/.maid/rules.rb
+++ home/.maid/rules.rb
@@ -1,4 +1,14 @@
 Maid.rules do
+  rule 'Old files downloaded while developing/testing' do
+    dir('~/Downloads/*').each do |path|
+      # if downloaded_from(path).any? { |u| u.match('http://localhost') || u.match('http://staging.yourcompany.com') } &&
+      if downloaded_from(path).any? { |u| u.match('http://localhost') } &&
+          1.week.since?(accessed_at(path))
+        trash(path)
+      end
+    end
+  end
+
   rule 'Move .torrent files on other machines to dropbox' do
     dir(['~/Downloads/*.torrent']).each do |path|
       move(path, '~/Dropbox/torrents')
