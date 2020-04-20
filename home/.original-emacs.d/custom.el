60429249b0c65016fc8f3b5192471e78c8267edc
Jeff Schoolcraft <jeff@thequeue.net>
2020-04-20 12:37:29 -0400

    Moving my current emacs config out of the way
    Going to install chemacs and doom emacs

---
+++ home/.original-emacs.d/custom.el
@@ -0,0 +1,40 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ag-highlight-search t t)
+ '(ag-reuse-buffers t t)
+ '(ag-reuse-window t t)
+ '(doom-themes-enabled-bold t t)
+ '(doom-themes-enabled-italic t t)
+ '(evil-collection-setup-minibuffer t)
+ '(flycheck-display-errors-delay nil)
+ '(flycheck-highlighting-mode (quote lines))
+ '(flycheck-idle-change-delay 2)
+ '(flycheck-indication-mode nil)
+ '(ivy-initial-inputs-alist nil)
+ '(load-prefer-newer t)
+ '(org-default-notes-file "~/Dropbox/org/notes.org")
+ '(org-directory "~/Dropbox/org")
+ '(org-log-done t)
+ '(org-outline-path-complete-in-steps nil)
+ '(org-refile-allow-creating-parent-nodes (quote confirm))
+ '(org-refile-targets (quote ((org-agenda-files :maxlevel . 4))))
+ '(org-refile-use-outline-path (quote file))
+ '(org-src-fontify-natively t)
+ '(org-src-tab-acts-natively t)
+ '(package-selected-packages
+   (quote
+    (linum-relative golden-ratio spaceline powerline-evil powerline doom-themes htmlize ox-pandoc ox-twbs org-web-tools toc-org org-bullets web-mode js2-mode json-mode ruby-refactor bundler haml-mode yaml-mode markdown-mode expand-region editorconfig deft bug-hunter flycheck-pos-tip flycheck restclient company counsel-projectile projectile avy ivy-hydra counsel ivy smex which-key ag evil-magit magit hydra evil-escape evil-org evil-collection evil-matchit evil-indent-textobject evil-surround evil-commentary evil-leader restart-emacs auto-compile diminish use-package)))
+ '(uniquify-after-kill-buffer-p t)
+ '(uniquify-buffer-name-style (quote post-forward) nil (uniquify))
+ '(uniquify-strip-common-suffix t)
+ '(use-package-always-ensure t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(flycheck-error ((((class color)) (:underline "Red"))))
+ '(flycheck-warning ((((class color)) (:underline "Orange")))))
---
+++ home/.original-emacs.d/elpa/counsel-projectile-20200130.1029/counsel-projectile-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "counsel-projectile" "20200130.1029" "Ivy integration for Projectile" '((counsel "0.13.0") (projectile "2.0.0")) :commit "47fcb4e0ebc9c9a3c47622912a00189005b092a3" :keywords '("project" "convenience") :authors '(("Eric Danan")) :maintainer '("Eric Danan") :url "https://github.com/ericdanan/counsel-projectile")
