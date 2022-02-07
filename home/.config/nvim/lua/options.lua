3783e0a87cc107f36284b3e9e68fb3284455d577
Jeff Schoolcraft <jeffrey.schoolcraft@gmail.com>
2022-02-07 06:50:21 -0500

    Set spell in gitcommit messages

--- home/.config/nvim/lua/options.lua
+++ home/.config/nvim/lua/options.lua
@@ -87,6 +87,9 @@ vim.api.nvim_exec(
     " For all text files set 'textwidth' to 78 characters.
     autocmd FileType text setlocal textwidth=108
 
+    " For gitcommit files set spell and textwidth
+    autocmd FileType text setlocal spell textwidth=72
+
     " When editing a file, always jump to the last known cursor position.
     " Don't do it when the position is invalid or when inside an event handler
     " (happens when dropping a file on gvim).
