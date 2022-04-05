64583014a93d0168e726fde28033b09232dfc8d4
Jeff Schoolcraft <jeffrey.schoolcraft@gmail.com>
2022-04-05 17:51:32 -0400

    Add mappings for hop

--- home/.config/nvim/lua/config/hop.lua
+++ home/.config/nvim/lua/config/hop.lua
@@ -2,3 +2,7 @@ require("hop").setup {
   keys = "etovxqpdygfblzhckisuran",
   term_seq_bias = 0.5,
 }
+
+local map = vim.api.nvim_set_keymap
+map("n", "<leader>hw", ":HopWord<CR>", { silent = true })
+map("n", "<leader>hw", ":HopLine<CR>", { silent = true })
