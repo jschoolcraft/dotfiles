10754b57bb9f20538ba5f8ef077dc16e421689d5
Jeff Schoolcraft <jeffrey.schoolcraft@gmail.com>
2022-04-21 10:28:31 -0400

    Enable current line blame by default

--- home/.config/nvim/lua/config/gitsigns.lua
+++ home/.config/nvim/lua/config/gitsigns.lua
@@ -1,1 +1,12 @@
-require("gitsigns").setup({})
+require("gitsigns").setup({
+  current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+  current_line_blame_opts = {
+    virt_text = true,
+    virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+    delay = 1000,
+    ignore_whitespace = false,
+  },
+  current_line_blame_formatter_opts = {
+    relative_time = false,
+  },
+})
