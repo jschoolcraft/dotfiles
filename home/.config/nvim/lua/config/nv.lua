465f82f880167c62180e76591239c45fae5b1938
Jeff Schoolcraft <jeffrey.schoolcraft@gmail.com>
2023-01-25 08:28:59 -0500

    Removing notational-fzf-vim
    I mostly just search for notes directly using telescope and then edit
    them.

--- home/.config/nvim/lua/config/nv.lua
+++
@@ -1,7 +0,0 @@
--- https://github.com/alok/notational-fzf-vim
-local g = vim.g
-g.nv_search_paths = {
-  '~/Dropbox/jschoolcraft/obsidian',
-  '~/Dropbox/jschoolcraft/notes',
-}
-g.nv_default_extension = '.md'
