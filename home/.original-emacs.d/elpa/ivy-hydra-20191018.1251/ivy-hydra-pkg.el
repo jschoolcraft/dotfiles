31e190a4e4a222da2b60f739b25f660c73ae9130
Jeff Schoolcraft <jeffrey.schoolcraft@gmail.com>
2023-10-22 18:40:09 -0400

    Removing emacs

--- home/.original-emacs.d/elpa/ivy-hydra-20191018.1251/ivy-hydra-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "ivy-hydra" "20191018.1251" "Additional key bindings for Ivy" '((emacs "24.5") (ivy "0.13.0") (hydra "0.15.0")) :commit "82331ed4accaa032419257e659926584beb76261" :keywords '("convenience") :authors '(("Oleh Krehel" . "ohwoeowho@gmail.com")) :maintainer '("Oleh Krehel" . "ohwoeowho@gmail.com") :url "https://github.com/abo-abo/swiper")
--- home/.original-emacs.d/elpa/request-20200127.21/request-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "request" "20200127.21" "Compatible layer for URL request in Emacs" '((emacs "24.4")) :commit "5bd2fbbf67ab1b50da357daa817549cd737dd2c9" :authors '(("Takafumi Arakaki <aka.tkf at gmail.com>")) :maintainer '("Takafumi Arakaki <aka.tkf at gmail.com>") :url "https://github.com/tkf/emacs-request")
