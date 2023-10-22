44bf7d18ff7378b195dbf0c892dd5ef7a949567c
Jeff Schoolcraft <jeffrey.schoolcraft@gmail.com>
2023-10-21 20:53:35 -0400

    Cleanup vim-pandoc-syntax

--- home/.config/nvim/lua/plugins/vim-pandoc-syntax.lua
+++ home/.config/nvim/lua/plugins/vim-pandoc-syntax.lua
@@ -1,13 +1,7 @@
 return {
-  -- writing, notes and things
-  'vim-pandoc/vim-pandoc',
-
-  {
-    'rwxrob/vim-pandoc-syntax-simple',
-    -- vim-pandoc's version has funky heading symbols
-    -- 'vim-pandoc/vim-pandoc-syntax',
-    dependencies = {
-      { 'vim-pandoc/vim-pandoc' }
-    }
+  'rwxrob/vim-pandoc-syntax-simple',
+  dependencies = {
+    { 'vim-pandoc/vim-pandoc' }
   },
+  ft = { "markdown", "pandoc" },
 }
