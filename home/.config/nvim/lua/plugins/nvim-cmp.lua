70cfd103037759a6e13f04340176208a1f6095d5
Jeff Schoolcraft <jeffrey.schoolcraft@gmail.com>
2023-10-21 20:55:08 -0400

    LSP and nvim-cmp fixes. Maybe.

--- home/.config/nvim/lua/plugins/nvim-cmp.lua
+++ home/.config/nvim/lua/plugins/nvim-cmp.lua
@@ -1,16 +1,44 @@
 return  {
-  -- Autocompletion
   'hrsh7th/nvim-cmp',
+  config = function ()
+    local cmp = require('cmp')
+    local luasnip = require('luasnip')
+
+    vim.opt.completeopt = 'menu,menuone,noselect'
+
+    cmp.setup({
+      snippet = {
+        expand = function(args)
+          require('luasnip').lsp_expand(args.body)
+        end,
+      },
+      mapping = cmp.mapping.preset.insert({
+        ["<C-k>"] = cmp.mapping.select_prev_item(),
+        ["<C-j>"] = cmp.mapping.select_next_item(),
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<C-e>"] = cmp.mapping.abort(),
+        ["<CR>"] = cmp.mapping.confirm({
+          behavior = cmp.ConfirmBehavior.Replace,
+          select = true,
+        }),
+      }),
+      sources = cmp.config.sources({
+        { name = 'nvim_lsp' },
+        { name = 'luasnip' },
+      }, {
+        { name = 'buffer' },
+      }),
+    })
+  end,
   dependencies = {
-    -- Snippet Engine & its associated nvim-cmp source
-    'L3MON4D3/LuaSnip',
-    'saadparwaiz1/cmp_luasnip',
-
-    -- Adds LSP completion capabilities
-    'hrsh7th/cmp-nvim-lsp',
-
-    -- Adds a number of user-friendly snippets
-    'rafamadriz/friendly-snippets',
+    { "L3MON4D3/LuaSnip" },
+    { "hrsh7th/cmp-path" },
+    { "hrsh7th/cmp-buffer" },
+    { "hrsh7th/cmp-cmdline" },
+    { "hrsh7th/cmp-nvim-lsp" },
+    { "saadparwaiz1/cmp_luasnip" },
+    { "kristijanhusak/vim-dadbod-completion" },
+    { "rafamadriz/friendly-snippets" },
   },
-  lazy = false,
+  event = { "InsertEnter", "CmdlineEnter" },
 }
