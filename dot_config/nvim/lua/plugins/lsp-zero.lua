89fb4d1b3358000db9ba21717d69eb1741acbf58
Jeff Schoolcraft <jeffrey.schoolcraft@gmail.com>
2024-02-04 20:49:54 -0500

    Missed lsp-zero

---
+++ dot_config/nvim/lua/plugins/lsp-zero.lua
@@ -0,0 +1,25 @@
+return {
+  {
+    "VonHeikemen/lsp-zero.nvim",
+    branch = "v3.x",
+    lazy = false,
+
+    dependencies = {
+      -- LSP Support
+      {'williamboman/mason.nvim'},
+      {'williamboman/mason-lspconfig.nvim'},
+      {'neovim/nvim-lspconfig'},
+
+      -- Autocompletion
+      {'hrsh7th/nvim-cmp'},
+      {'hrsh7th/cmp-nvim-lsp'},
+      {'hrsh7th/cmp-buffer'},
+      {'hrsh7th/cmp-path'},
+      {'saadparwaiz1/cmp_luasnip'},
+
+      -- Snippets
+      {'L3MON4D3/LuaSnip'},
+      {'rafamadriz/friendly-snippets'},
+    },
+  }
+}
