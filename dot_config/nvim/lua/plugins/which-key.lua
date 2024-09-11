20d97cc9ffaee1a71b96076247dc0d056af2b292
Jeff Schoolcraft <jeffrey.schoolcraft@gmail.com>
2024-09-11 13:06:29 -0400

    Fix which-key config
    Mainly go back to simple default, I don't have any real customizations.

--- dot_config/nvim/lua/plugins/which-key.lua
+++ dot_config/nvim/lua/plugins/which-key.lua
@@ -1,13 +1,19 @@
 return {
   "folke/which-key.nvim",
-  config = function()
-    require("which-key").setup {
-      plugins = {
-        marks = true,
-        registers = true,
-      },
-      ignore_missing = false,
-    }
-  end,
-  lazy = false,
+  event = "VeryLazy",
+  opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+  },
+  keys = {
+    {
+      "<leader>?",
+      function()
+        require("which-key").show({ global = false })
+      end,
+      desc = "Buffer Local Keymaps (which-key)",
+    },
+  },
 }
+-- https://github.com/folke/which-key.nvim
