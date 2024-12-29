9cd48e084293c8850b230381a3f358fc4d9af147
Jeff Schoolcraft <jeffrey.schoolcraft@gmail.com>
2024-12-29 09:20:58 -0500

    Updating the colorscheme
    I never really change schemes, so let's just stick with one.
    
    Updated to toggle between light and dark. Not sure I'll ever use that
    but currently ghostty is set to match light/dark from macOS.

--- dot_config/nvim/lua/config/plugins/colorscheme.lua
+++ dot_config/nvim/lua/config/plugins/colorscheme.lua
@@ -1,24 +1,29 @@
 return {
-  { "navarasu/onedark.nvim", },
   {
-    "olimorris/onedarkpro.nvim",
-    lazy = false,
+    "navarasu/onedark.nvim",
     priority = 999,
     config = function()
-      require("onedarkpro").setup({
-        styles = {
-          comments = "italic",
-          functions = "italic",
-          keywords = "italic",
-          strings = "italic",
-          variables = "italic",
+      require("onedark").setup({
+        style ="darker",
+        term_colors = true,
+
+        -- toggle theme style ---
+        toggle_style_key = "<leader>ts", -- keybind to toggle theme style. Leave it nil to disable it, or set it to a string, for example "<leader>ts"
+        toggle_style_list = {'light', 'darker'}, -- List of styles to toggle between
+        -- toggle_style_list = {'dark', 'darker', 'cool', 'deep', 'warm', 'warmer', 'light'}, -- List of styles to toggle between
+
+        -- Change code style ---
+        -- Options are italic, bold, underline, none
+        -- You can configure multiple style with comma separated, For e.g., keywords = 'italic,bold'
+        code_style = {
+          comments = 'italic',
+          keywords = 'bold',
+          functions = 'bold',
+          strings = 'none',
+          variables = 'none'
         },
       })
-      vim.cmd.colorscheme "onedark_dark"
+      require("onedark").load()
     end,
   },
-  { "arcticicestudio/nord-vim", },
-  { "rakr/vim-one", },
-  { "sainnhe/sonokai", },
-  { 'EdenEast/nightfox.nvim' },
 }
