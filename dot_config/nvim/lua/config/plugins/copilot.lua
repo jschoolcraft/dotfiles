e803796c9be2a0347298e0820ec6748c6451e10e
Jeff Schoolcraft <jeffrey.schoolcraft@gmail.com>
2025-07-01 20:43:41 -0400

    Switch to lua copilot and shimmed node

--- dot_config/nvim/lua/config/plugins/copilot.lua
+++ dot_config/nvim/lua/config/plugins/copilot.lua
@@ -1,15 +1,12 @@
 return {
-  "github/copilot.vim",
-  lazy = false
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    config = function()
+      require("copilot").setup({
+        copilot_node_command = vim.fn.expand("~/.local/share/mise/installs/node/20.17.0/bin/node"),
+      })
+    end,
+  }
 }
-
--- return {
---   {
---     "zbirenbaum/copilot.lua",
---     cmd = "Copilot",
---     event = "InsertEnter",
---     config = function()
---       require("copilot").setup()
---     end,
---   }
--- }
