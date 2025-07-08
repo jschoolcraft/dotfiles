c2f1e0433bdefb6777d1936cc57a11447bd44d18
Jeff Schoolcraft <jeffrey.schoolcraft@gmail.com>
2025-07-08 19:20:00 -0400

    Adding a few more snacks to neovim

--- dot_config/nvim/lua/config/plugins/snacks.lua
+++ dot_config/nvim/lua/config/plugins/snacks.lua
@@ -13,10 +13,16 @@ return {
       scroll = { enabled = false },
       statuscolumn = { enabled = true },
       words = { enabled = true },
+      terminal = { enabled = true },
+      zen = { enabled = true },
+      scratch = { enabled = true },
     },
     keys = {
       { "<leader>gb", function() Snacks.git.blame_line() end, desc = "[G]it [B]lame line" },
       { "<leader>lg", function() Snacks.lazygit() end, desc = "[L]azy [G]it" },
+      { "<leader>tt", function() Snacks.terminal() end, desc = "[T]erminal [T]oggle" },
+      { "<leader>tz", function() Snacks.zen() end, desc = "[T]oggle [Z]en mode" },
+      { "<leader>.", function() Snacks.scratch() end, desc = "Scratch buffer" },
     },
   }
 }
