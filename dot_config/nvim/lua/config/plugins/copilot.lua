a863889f1983f8c83b3e74dd192a4feda8ee1f32
Jeff Schoolcraft <jeffrey.schoolcraft@kaleris.com>
2026-02-18 11:25:40 -0500

    Remove AI plugins from nvim config

--- dot_config/nvim/lua/config/plugins/copilot.lua
+++ dot_config/nvim/lua/config/plugins/copilot.lua
@@ -1,12 +1,1 @@
-return {
-  {
-    "zbirenbaum/copilot.lua",
-    cmd = "Copilot",
-    event = "InsertEnter",
-    config = function()
-      require("copilot").setup({
-        copilot_node_command = vim.fn.system("mise where node@20"):gsub("\n", "") .. "/bin/node",
-      })
-    end,
-  }
-}
+return {}
